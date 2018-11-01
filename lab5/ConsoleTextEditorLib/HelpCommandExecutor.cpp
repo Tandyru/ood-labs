26da4c0c0972de5f30a810881ebdd6a3076eceee
Tandyru <43044294+Tandyru@users.noreply.github.com>
2018-11-01 21:21:47 +0300

    lab5 help command was implemented

--- lab5/ConsoleTextEditorLib/HelpCommandExecutor.cpp
+++ lab5/ConsoleTextEditorLib/HelpCommandExecutor.cpp
@@ -7,8 +7,19 @@ namespace input_command
 
 void ShowCommandHelp(ostream& out)
 {
-	// TODO:
-	out << "Available commands:\n TODO:";
+	out << "Available commands:\n"
+		"  InsertParagraph <позиция>|end <текст параграфа>\n"
+		"  InsertImage <позиция>|end <ширина> <высота> <путь к файлу изображения>\n"
+		"  SetTitle <заголовок документа>\n"
+		"  ReplaceText <позиция> <текст параграфа>\n"
+		"  ResizeImage <позиция> <ширина> <высота>\n"
+		"  DeleteItem <позиция>\n"
+		"  Undo\n"
+		"  Redo\n"
+		"  List\n"
+		"  Help\n"
+		//"  Save <путь>\n"
+		;
 }
 
 }
